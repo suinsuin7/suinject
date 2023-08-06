2c811cb6750cd71dc1f1a977b4dc90b547ca6637
suin0704 <fropop74@gmail.com>
2023-08-06 16:23:37 +0900

    0806 뉴렉처 mvc 15~18

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -10,7 +10,7 @@ public class ListController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView("notice/list");
+		ModelAndView mv = new ModelAndView("notice.list");
 		
 		return mv;
 	}
