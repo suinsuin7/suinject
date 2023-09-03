db2d34ebb2f3359a550fe4ce329eacdfa3052f9e
suin0704 <fropop74@gmail.com>
2023-09-03 15:45:15 +0900

    0903 풀캘린더 공부

--- fullcalendar/src/main/java/full/calendar/test/controller/ScheduleController.java
+++ fullcalendar/src/main/java/full/calendar/test/controller/ScheduleController.java
@@ -1,10 +1,19 @@
 package full.calendar.test.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import full.calendar.test.service.ScheduleService;
+import full.calendar.test.vo.ScheduleVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,5 +30,29 @@ public class ScheduleController {
 	}
 	
 	//이벤트 조회
+	// 이벤트 조회
+		@GetMapping("/calendar-admin-update")
+		@ResponseBody
+		public List<Map<String, Object>> showAllEventInUpdate() throws Exception{
+			
+			JSONObject jsonObj = new JSONObject();
+			JSONArray jsonArr = new JSONArray();
+			
+			HashMap<String, Object> hash = new HashMap<>();
+			
+			List<ScheduleVO> list = scheduleService.findAll();
+			
+			for(ScheduleVO schedule : list) {
+				hash.put("id", schedule.getScheduleId());
+				hash.put("title", schedule.getCreatorId());
+				hash.put("start", schedule.getScheduleDateTimeStart());
+				hash.put("end", schedule.getScheduleDateTimeEnd());
+				
+				jsonObj = new JSONObject(hash);
+				jsonArr.add(jsonObj);
+			}
+			log.info("jsonArrCheck:{}",jsonArr);
+			return jsonArr;
+		}
 	
 }
